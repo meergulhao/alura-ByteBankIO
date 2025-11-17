ec894f09b08f827e9d26705a9b94bb5448e00abf
Guilherme Mergulhão <golimpiof@gmail.com>
2025-11-17 16:21:20 -0300

    Simplifica criação do arquivo com StreamWriter

--- ByteBankIO/3_CriandoArquivo.cs
+++ ByteBankIO/3_CriandoArquivo.cs
@@ -6,8 +6,8 @@ partial class Program
     static void CriarArquivo()
     {
         var caminhoNovoArquivo = "contasExportadas.csv";
-        
-        using(var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+
+        using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
         {
             var contaComoString = "456, 7895, 4785.40, Guilherme Mergulhão";
 
@@ -16,6 +16,17 @@ partial class Program
             var bytes = encoding.GetBytes(contaComoString);
 
             fluxoDeArquivo.Write(bytes, 0, bytes.Length);
+        }
+    }
+    
+    static void CriarArquivoComWriter()
+    {
+        var caminhoNovoArquivo = "contasExportadas.csv";
+
+        using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+        using (var escritor = new StreamWriter(fluxoDeArquivo))
+        {
+            escritor.Write("123, 6528, 3406.20, Guilherme Mergulhão");
         }
     }
 }

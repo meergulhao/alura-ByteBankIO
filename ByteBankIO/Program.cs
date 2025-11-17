b6c5be6e8aff3fa921a5dfb5861fb3a60f194f7d
Guilherme Mergulhão <golimpiof@gmail.com>
2025-11-17 13:46:05 -0300

    Primeiro commit

---
+++ ByteBankIO/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using ByteBankIO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var enderecoDoArquivo = "contas.txt";
+        var numeroDeBytesLidos = -1;
+
+        var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
+
+        var buffer = new byte[1024]; //1KB
+
+        while(numeroDeBytesLidos != 0)
+        {
+            numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+            EscreverBuffer(buffer);
+        }
+
+        Console.ReadLine();
+    }
+
+    static void EscreverBuffer(byte[] buffer)
+    {
+        var utf8 = new UTF8Encoding();
+
+        var texto = utf8.GetString(buffer);
+        System.Console.Write(texto);
+
+        // foreach (var meuByte in buffer)
+        // {
+        //     System.Console.Write(meuByte);
+        //     System.Console.Write(" ");
+        // }
+    }
+}
